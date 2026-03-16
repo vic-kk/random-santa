263d38009580eba3bfff0d31bcd2b0b83a7297fd
Victor K <dev@victorkarasev.ru>
2026-03-17 05:06:02 +0700

    form prefill

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,5 +2,5 @@ type TExternalLinks = Record<'tgAdmin' | 'googleForm' , string>
 
 export const URLS:TExternalLinks = {
   tgAdmin: 'https://t.me/+omk7AIuSRmZmMjMy7',
-  googleForm: 'https://forms.gle/ichPY3Vzn1FY5fUL9',
+  googleForm: 'https://docs.google.com/forms/d/e/1FAIpQLSdGnfGgwdcrFjc1ex4wWfqXuF7KC_reem0wZw_yQWgqfeyx2Q/viewform?usp=pp_url&entry.1901592428=',
 };
