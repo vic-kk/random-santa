c6f37f0a6e68e2d8034b33cd6e2ec1e3ffbfdd6e
Victor K <dev@victorkarasev.ru>
2026-03-17 15:31:01 +0700

    add form prefill posibility

--- src/containers/GoogleForm/GoogleForm.tsx
+++ src/containers/GoogleForm/GoogleForm.tsx
@@ -2,11 +2,15 @@ import { EXTERNAL_LINKS } from 'src/data';
 import { useSantaId } from 'src/hooks';
 import './GoogleForm.css'
 
+function processGoogleFormUrl(url: string, uid: string) {
+  return url.includes('pp_url&entry') ? url.concat(uid) : url;
+}
+
 const GoogleForm = () => {
   const uid = useSantaId();
 
   return (
-    <iframe className='form' src={EXTERNAL_LINKS.googleForm+uid}>Загрузка…</iframe>
+    <iframe className='form' src={processGoogleFormUrl(EXTERNAL_LINKS.googleForm, uid)}>Загрузка…</iframe>
   )
 }
 
