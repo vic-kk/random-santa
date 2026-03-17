992491b6d724c491711639ba8a0b3e61dc2dc961
Victor K <dev@victorkarasev.ru>
2026-03-17 15:29:40 +0700

    add feature descriptions

--- src/features/features.ts
+++ src/features/features.ts
@@ -1,4 +1,10 @@
 export const FEATURES = {
+  /**
+   * Display a service message, hide the form and results 
+   */
   IN_SERVICE: false,
+  /**
+   * true - display the result, false - display the Google form
+   */
   SANTA_READY: true,
 }
